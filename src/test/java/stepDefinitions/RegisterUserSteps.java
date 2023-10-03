6eaf72f81499b433a38c696d24c327d2e670479f
sayed <sayed.sadatibrahim@gmail.com>
2023-10-03 14:20:17 -0700

    changes

--- src/test/java/stepDefinitions/RegisterUserSteps.java
+++ src/test/java/stepDefinitions/RegisterUserSteps.java
@@ -1,5 +1,51 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class RegisterUserSteps {
+
+@Given("user is on home page")
+public void user_is_on_home_page() {
+    
+}
+
+@When("user clicks on myaccount")
+public void user_clicks_on_myaccount() {
+  
+}
+
+@When("user click register button")
+public void user_click_register_button() {
+    
+}
+
+@When("user fill out the below information")
+public void user_fill_out_the_below_information(io.cucumber.datatable.DataTable dataTable) {
+    
+}
+
+@When("User select {string} for Subscribe")
+public void user_select_for_subscribe(String string) {
+   
+}
+
+@When("User click privacy and policy button")
+public void user_click_privacy_and_policy_button() {
+   
+}
+
+@When("User click Continue button")
+public void user_click_continue_button() {
+    
+}
+
+@Then("user account is created with a message as follows: {string}")
+public void user_account_is_created_with_a_message_as_follows(String string) {
+   
+}
+
+
 
 }
